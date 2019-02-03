132fa73110cfc6c4252a0fae668706890b085685
cojuer <cojuer@gmail.com>
2019-02-03 16:22:58 +0300

    Add utils and unit tests for them

---
+++ src/utils.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import json
+from dataclasses import dataclass
+
+@dataclass
+class TcQuality:
+    bandwidth: float
+    delay: int
+    jitter: float
+    loss: float
+
+    def __post_init__(self):
+        self.bandwidth = float(self.bandwidth)
+        self.delay = int(self.delay)
+        self.jitter = float(self.jitter)
+        self.loss = float(self.loss)
+
+    def to_tuple(self) -> tuple:
+        return self.bandwidth, self.delay, self.jitter, self.loss
+
+@dataclass
+class Quality:
+    bandwidth: float
+    delay: float
+    jitter: float
+    loss: float
+
+    def __post_init__(self):
+        self.bandwidth = float(self.bandwidth)
+        self.delay = float(self.delay)
+        self.jitter = float(self.jitter)
+        self.loss = float(self.loss)
+
+    def __hash__(self):
+        return hash(str(self))
+
+    def __eq__(self, rhs):
+        return (self.bandwidth, self.delay, self.jitter, self.loss) == \
+            (rhs.bandwidth, rhs.delay, rhs.jitter, rhs.loss)
+
+    def to_tc_quality(self) -> TcQuality:
+        return TcQuality(self.bandwidth / 8, self.delay, 
+                         self.delay * self.jitter / 100, self.loss)
+
+    @staticmethod
+    def from_tc_quality(tc_quality: TcQuality) -> 'Quality':
+        return Quality(
+            bandwidth = tc_quality.bandwidth * 8,
+            delay = tc_quality.delay,
+            jitter = tc_quality.jitter / tc_quality.delay * 100,
+            loss = tc_quality.loss
+        )
+
+    def to_tuple(self) -> tuple:
+        return self.bandwidth, self.delay, self.jitter, self.loss
+
+def parse_server_out(file_path: str) -> int:
+    """ Opens iperf server output and reads bits_per_second 
+    """
+    with open(file_path, 'r') as fp:
+        file_contents = ''
+        for line in fp:
+            file_contents += line
+            if line == '}\n':
+                break
+        attempt_res = json.loads(file_contents)
+        speed = attempt_res['end']['sum_received']['bits_per_second']
+    return speed
+
+
+def parse_view(file_path: str) -> (float, int):
+    """ Probably parses CLI output with socket statistics
+    """
+    with open(file_path, 'r') as fp:
+        content = fp.readlines()
+        acks_num = int(content[1].split(':')[1])
+        loss_num = int(content[2].split(':')[1])
+        rtt = int(content[3].split(':')[1])
+    return (1.0 * loss_num / acks_num, rtt)
+
+
+def parse_res(file_path: str) -> dict:
+    """ Opens file with optimal algorithms, parses 
+        and returns data per quality characteristics
+    """
+    res = dict()
+    with open(file_path, 'r') as fp:
+        for line in fp:
+            if len(line) == 0: 
+                break
+            bw, delay, jitter, loss, _, alg = \
+                [elem.strip() for elem in line[:-1].split(',')]
+            quality = Quality(bw, delay, jitter, loss)
+            res[quality] = alg
+    print(res)
+    return res
+
+
+def parse_res_per_alg(file_path: str) -> dict:
+    """ Opens file with algorithm performance per quality, parses
+        and returns data per quality charateristics
+    """
+    res = dict()
+    with open(file_path, 'r') as fp:
+        for line in fp:
+            if len(line) == 0: 
+                break
+            bw, delay, jitter, loss, speed = \
+                [elem.strip() for elem in line[:-1].split(',')]
+            quality = Quality(bw, delay, jitter, loss)
+            res[quality] = float(speed)
+    return res
+
+def get_srv_out_name(alg: str, tc_quality: TcQuality, attempt: int) -> str:
+    assert isinstance(tc_quality, TcQuality)
+    bw, delay, jitter, loss = tc_quality.to_tuple()
+    loss = int(loss) if loss == 1 else loss
+    return 'net_{}_{}_{}_{}_{}_{}_server.json'\
+        .format(alg, bw, delay, jitter, loss, attempt)
---
+++ tests/test_utils.py
@@ -0,0 +1,67 @@
+import os
+import pytest
+from src.utils import *
+
+dirname = os.path.dirname(os.path.abspath(__file__))
+
+def test_init_quality():
+    q = Quality(1, 2, 3, 4)
+    assert q.bandwidth == 1
+    assert q.delay == 2
+    assert q.jitter == 3
+    assert q.loss == 4
+
+def test_quality_to_tc():
+    q = Quality(1, 2, 3, 4)
+    tc_q = q.to_tc_quality()    
+    assert tc_q.bandwidth == 1 / 8
+    assert tc_q.delay == 2
+    assert tc_q.jitter == 3 * 2 / 100
+    assert tc_q.loss == 4
+
+def test_quality_from_tc():
+    q = Quality(1, 2, 3, 4)
+    tc_q = q.to_tc_quality()
+    q2 = Quality.from_tc_quality(tc_q)
+    assert q == q2
+
+def test_quality_to_tuple():
+    q = Quality(1, 2, 3, 4)
+    assert q.to_tuple() == (1, 2, 3, 4)
+
+def test_parse_srv_out():
+    speed = parse_server_out(
+        os.path.join(dirname, 'test_data/srv_out.json')
+    )
+    assert speed == 961361.430820
+
+def test_parse_view():
+    res = parse_view(
+        os.path.join(dirname, 'test_data/view_out.txt')
+    )
+    assert res == (0, 58995)
+
+def test_parse_res():
+    res = parse_res(
+        os.path.join(dirname, 'test_data/res_out.csv')
+    )
+    assert len(res) > 0
+    assert res[Quality(1, 1, 0, 0.0001)] == 'reno'
+
+def test_parse_res_per_alg():
+    res = parse_res_per_alg(
+        os.path.join(dirname, 'test_data/res_per_alg_out.csv')
+    )
+    assert len(res) > 0
+    assert res[Quality(1, 1, 0, 0.0001)] - 953836.6202723333 < 0.00001
+
+@pytest.mark.parametrize('alg,quality,attempt,expected', [
+        ('bbr', Quality(1, 1, 20, 0.001), 0, 'net_bbr_0.125_1_0.2_0.001_0_server.json'),
+        ('bbr', Quality(1, 1, 20, 1.0), 1, 'net_bbr_0.125_1_0.2_1_1_server.json'),
+        ('cubic', Quality(1, 1, 0, 0.001), 2, 'net_cubic_0.125_1_0.0_0.001_2_server.json'),
+    ])
+def test_srv_out_name(alg, quality, attempt, expected):
+    tc_q = quality.to_tc_quality()
+    name = get_srv_out_name(alg, tc_q, attempt)
+    assert name == expected
+
